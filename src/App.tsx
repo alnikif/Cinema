975678f22c9f4051ff912d688c8669729285b70a
alnikif <nikiforiakoleksandr@gmail.com>
2023-11-27 20:37:22 +0100

    Finished Initiate an application

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App" />;
-}
+const App = () => {
+  return <div className="App">hello</div>;
+};
 
 export default App;
