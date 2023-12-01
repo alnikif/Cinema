c006e29bbbdbc321c04aa9fe32e444e0b4bce014
alnikif <nikiforiakoleksandr@gmail.com>
2023-12-01 07:37:02 +0100

    create Card component

--- src/pages/RickAndMorty/RickAndMortyCharacter.tsx
+++ src/pages/RickAndMorty/RickAndMortyCharacter.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { CellType } from '../../components/Table/CellType';
 import { useParams } from 'react-router-dom';
-import { inspect } from 'util';
 import styles from './RickAndMortyCharacter.module.scss';
+import { Card } from '../../components/Cards/Card/Card';
 
 export type RickAndMortyType = {
   id: number;
@@ -15,17 +15,6 @@ export type RickAndMortyType = {
   status: string;
   type: string;
 };
-
-const headerRickAndMortyRowConfig = [
-  { key: 'id', label: 'Id', cellType: CellType.link, width: 1 },
-  { key: 'name', label: 'Character name', cellType: CellType.name, width: 3 },
-  { key: 'gender', label: 'Gender', cellType: CellType.gender, width: 2 },
-  { key: 'image', label: 'Image', cellType: CellType.image, width: 1 },
-  { key: 'species', label: 'Species', cellType: CellType.species, width: 2 },
-  { key: 'location', label: 'Location', cellType: CellType.location, width: 2 },
-  { key: 'status', label: 'Status', cellType: CellType.status, width: 2 },
-  { key: 'type', label: 'Type', cellType: CellType.type, width: 2 }
-];
 
 export const RickAndMortyCharacter = () => {
   const [rickAndMortyCharacter, setRickAndMortyCharacter] = useState<RickAndMortyType | null>(null);
@@ -58,20 +47,10 @@ export const RickAndMortyCharacter = () => {
   if (!rickAndMortyCharacter) {
     return <div>No data</div>;
   }
-  const { id, name, gender, image, species, location, status, type } = rickAndMortyCharacter;
 
   return (
     <div className={styles.CharacterWrapper}>
-      <div className={styles.CharacterCard}>
-        <img src={image} alt={name} />
-        <div>{name}</div>
-        <div className={styles.CharacterInfo}>
-          <div>{gender}</div>
-          <div>{species}</div>
-          <div>{status}</div>
-        </div>
-        <div>Location: {location.name}</div>
-      </div>
+      <Card characterData={rickAndMortyCharacter} />
     </div>
   );
 };
