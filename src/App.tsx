15761de8d9e92b47e19b947cacf782c49a985a57
alnikif <nikiforiakoleksandr@gmail.com>
2024-01-22 22:16:11 +0100

    add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import { Layout } from './components/Layout/Layout';
+import LayoutWrapper from './components/Layout/Layout';
 import Providers from './Providers';
 
 const App = () => {
   return (
     <Providers>
       <>
-        <Layout />
+        <LayoutWrapper />
         <Outlet />
       </>
     </Providers>
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
+// import './index.css';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 
-import { Header } from './Header/Header';
+const { Header, Content, Footer } = Layout;
+
+
+
+// import { Header } from './Header/Header';
 import Dropdown from '../Dropdown/Dropdown';
 import { ThemeContext } from '../../Providers/ThemeProvider';
 import { themes } from '../../constants/theme';
 import styles from './Layout.module.scss';
+import {Link} from "react-router-dom";
 
 export enum NavItems {
   homePage = 'Home Page',
@@ -27,22 +34,79 @@ export const navItemsConfig = [
   { label: NavItems.formPlayground, url: '/form-playground' }
 ];
 
-export const Layout = () => {
-  const themesOptions = themes.map(({ key, title }) => ({
-    id: key,
-    label: title
-  }));
+// export const Layout = () => {
+//   const themesOptions = themes.map(({ key, title }) => ({
+//     id: key,
+//     label: title
+//   }));
+//
+//   const { theme, setTheme: onChangeTheme } = useContext(ThemeContext);
+//
+//   return (
+//       <div className={styles.layoutWrapper}>
+//         <div className={styles.headerWrapper}>
+//           <Header navItemsConfig={navItemsConfig} />
+//         </div>
+//         <div className={styles.dropdownThemeWrapper}>
+//           <Dropdown selectedOptionId={theme} options={themesOptions} onSelect={onChangeTheme} />
+//         </div>
+//       </div>
+//   );
+// };
 
-  const { theme, setTheme: onChangeTheme } = useContext(ThemeContext);
+
+
+const items = new Array(3).fill(null).map((_, index) => ({
+  key: String(index + 1),
+  label: `nav ${index + 1}`,
+}));
+
+const LayoutWrapper: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
   return (
-      <div className={styles.layoutWrapper}>
-        <div className={styles.headerWrapper}>
-          <Header navItemsConfig={navItemsConfig} />
-        </div>
-        <div className={styles.dropdownThemeWrapper}>
-          <Dropdown selectedOptionId={theme} options={themesOptions} onSelect={onChangeTheme} />
-        </div>
-      </div>
+      <Layout>
+        <Header
+            style={{
+              position: 'sticky',
+              top: 0,
+              zIndex: 1,
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+        >
+          <div className="demo-logo" />
+          <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              items={items}
+              style={{ flex: 1, minWidth: 0 }}
+          />
+        </Header>
+        <Content style={{ padding: '0 48px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            {navItemsConfig.map(el => <Breadcrumb.Item key={el.label} href={el.url}>{el.label}</Breadcrumb.Item>)}
+          </Breadcrumb>
+          <div
+              style={{
+                padding: 24,
+                minHeight: 380,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+          >
+            Content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
   );
 };
+
+export default LayoutWrapper;
