d89491f127b17a8ede5be70a7aaf5e335009e9b3
alnikif <nikiforiakoleksandr@gmail.com>
2024-01-24 17:18:17 +0100

    add ant design card for the star wars page

--- src/components/Cards/HarryPotterCards/Card/HarryPotterCard.tsx
+++ src/components/Cards/HarryPotterCards/Card/HarryPotterCard.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { HarryPotterType } from '../../../../types/harryPotterTypes';
 import { Link } from 'react-router-dom';
 import { Card } from 'antd';
+import cx from "classnames";
 import styles from './HarryPotterCard.module.scss';
 
 const { Meta } = Card;
@@ -10,9 +11,7 @@ const { Meta } = Card;
 export const HarryPotterCard: React.FC<{ readonly characterData: HarryPotterType }> = ({ characterData }) => {
   const { id, image, name, gender, species, wizard } = characterData;
   return (
-
-
-<>
+    <>
       <Card
           hoverable
           style={{ width: 240 }}
@@ -25,28 +24,28 @@ export const HarryPotterCard: React.FC<{ readonly characterData: HarryPotterType
               <div className={styles.species}>
                   {species}
               </div>
-              <div className={styles.wizard}>
+              <div className={cx(wizard && styles.wizard)}>
                   {wizard && 'wizard'}
               </div>
           </div>
       </Card>
 
-    {/*Custom card*/}
+        {/*Custom card*/}
 
-    {/*  */}
-    {/*<div className={styles.CharacterCard}>*/}
-    {/*  <img src={image} alt={name} />*/}
-    {/*  <div>*/}
-    {/*    <Link to={`/harry-potter/${id}`} className={styles.link}>*/}
-    {/*      {name}*/}
-    {/*    </Link>*/}
-    {/*  </div>*/}
-    {/*  <div className={styles.CharacterInfo}>*/}
-    {/*    <div>{gender}</div>*/}
-    {/*    <div>{species}</div>*/}
-    {/*    <div>{wizard ? 'wizard' : 'muggle'}</div>*/}
-    {/*  </div>*/}
-    {/*</div>*/}
-</>
+        {/*  */}
+        {/*<div className={styles.CharacterCard}>*/}
+        {/*  <img src={image} alt={name} />*/}
+        {/*  <div>*/}
+        {/*    <Link to={`/harry-potter/${id}`} className={styles.link}>*/}
+        {/*      {name}*/}
+        {/*    </Link>*/}
+        {/*  </div>*/}
+        {/*  <div className={styles.CharacterInfo}>*/}
+        {/*    <div>{gender}</div>*/}
+        {/*    <div>{species}</div>*/}
+        {/*    <div>{wizard ? 'wizard' : 'muggle'}</div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+    </>
   );
 };
--- src/components/Cards/StarWarsCards/Card/StarWarsCard.tsx
+++ src/components/Cards/StarWarsCards/Card/StarWarsCard.tsx
@@ -2,22 +2,48 @@ import React from 'react';
 import styles from './StarWarsCard.module.scss';
 import { StarWarsType } from '../../../../types/starWarsTypes';
 import { Link } from 'react-router-dom';
+import {Card} from "antd";
+
+const {Meta} = Card;
 
 export const StarWarsCard: React.FC<{ readonly characterData: StarWarsType }> = ({ characterData }) => {
   const { image, name, gender, species, homeworld, id } = characterData;
   return (
-    <div className={styles.CharacterCard}>
-      <img src={image} alt={name} />
-      <div>
-        <Link to={`/star-wars/${id}`} className={styles.link}>
-          {name}
-        </Link>
-      </div>
-      <div className={styles.CharacterInfo}>
-        <div>{gender}</div>
-        <div>{species}</div>
-        <div>{homeworld}</div>
-      </div>
-    </div>
+      <>
+          <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src={image} />}
+          >
+              <Meta title={<Link to={`/star-wars/${id}`} className={styles.link}>
+                  {name}
+              </Link>} description={gender} />
+              <div className={styles.CharacterInfo}>
+                  <div className={styles.species}>
+                      {species}
+                  </div>
+                  <div className={styles.homeworld}>
+                      {homeworld}
+                  </div>
+              </div>
+          </Card>
+
+          {/*Custom Card*/}
+
+        {/*<div className={styles.CharacterCard}>*/}
+        {/*  <img src={image} alt={name} />*/}
+        {/*  <div>*/}
+        {/*    <Link to={`/star-wars/${id}`} className={styles.link}>*/}
+        {/*      {name}*/}
+        {/*    </Link>*/}
+        {/*  </div>*/}
+        {/*  <div className={styles.CharacterInfo}>*/}
+        {/*    <div>{gender}</div>*/}
+        {/*    <div>{species}</div>*/}
+        {/*    <div>{homeworld}</div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+
+      </>
   );
 };
